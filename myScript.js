6d1aadc6ff7436c5abd45cf7fe1f814786979e3e
MrAPauls <55466660+MrAPauls@users.noreply.github.com>
2020-04-06 19:40:58 -0300

    uploaded files

---
+++ myScript.js
@@ -0,0 +1,15 @@
+	alert("My first Script!");  //First Alert
+
+	function ButtonAlert () {  //Function for Second Alert
+		alert("My first Script!");
+							}
+							
+	document.write("My Second Script!");   //doucument write
+	
+	function Button_inner() {  //innerHTML
+			document.getElementById("special").innerHTML="My Thrid Script!";
+							}
+	
+	console.log("My Fourth Script!")    //console.log
+
+
